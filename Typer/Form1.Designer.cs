b85f88e297aaa0ec3854f8f718699258d53d4fb7
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2020-11-18 10:40:17 +0700

    Initial commit

---
+++ Typer/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Typer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.RichTextBox();
+            this.txtText = new System.Windows.Forms.RichTextBox();
+            this.lblWpm = new System.Windows.Forms.Label();
+            this.grpMode = new System.Windows.Forms.GroupBox();
+            this.rbtnPractice = new System.Windows.Forms.RadioButton();
+            this.rbtnTest = new System.Windows.Forms.RadioButton();
+            this.grpResults = new System.Windows.Forms.GroupBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.grpMode.SuspendLayout();
+            this.grpResults.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(12, 250);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.ShortcutsEnabled = false;
+            this.txtUser.Size = new System.Drawing.Size(574, 44);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Text = "";
+            this.txtUser.TextChanged += new System.EventHandler(this.txtUser_TextChanged);
+            this.txtUser.Enter += new System.EventHandler(this.txtUser_Enter_1);
+            this.txtUser.Leave += new System.EventHandler(this.txtUser_Leave_1);
+            // 
+            // txtText
+            // 
+            this.txtText.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtText.Location = new System.Drawing.Point(12, 12);
+            this.txtText.Name = "txtText";
+            this.txtText.ShortcutsEnabled = false;
+            this.txtText.Size = new System.Drawing.Size(574, 195);
+            this.txtText.TabIndex = 2;
+            this.txtText.Text = "";
+            // 
+            // lblWpm
+            // 
+            this.lblWpm.AutoSize = true;
+            this.lblWpm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWpm.Location = new System.Drawing.Point(30, 31);
+            this.lblWpm.Name = "lblWpm";
+            this.lblWpm.Size = new System.Drawing.Size(27, 25);
+            this.lblWpm.TabIndex = 3;
+            this.lblWpm.Text = "...";
+            this.lblWpm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // grpMode
+            // 
+            this.grpMode.Controls.Add(this.rbtnTest);
+            this.grpMode.Controls.Add(this.rbtnPractice);
+            this.grpMode.Location = new System.Drawing.Point(592, 12);
+            this.grpMode.Name = "grpMode";
+            this.grpMode.Size = new System.Drawing.Size(151, 100);
+            this.grpMode.TabIndex = 4;
+            this.grpMode.TabStop = false;
+            this.grpMode.Text = "Mode";
+            // 
+            // rbtnPractice
+            // 
+            this.rbtnPractice.AutoSize = true;
+            this.rbtnPractice.Checked = true;
+            this.rbtnPractice.Location = new System.Drawing.Point(7, 22);
+            this.rbtnPractice.Name = "rbtnPractice";
+            this.rbtnPractice.Size = new System.Drawing.Size(80, 21);
+            this.rbtnPractice.TabIndex = 0;
+            this.rbtnPractice.TabStop = true;
+            this.rbtnPractice.Text = "Practice";
+            this.rbtnPractice.UseVisualStyleBackColor = true;
+            this.rbtnPractice.Click += new System.EventHandler(this.rbtnPractice_Click);
+            // 
+            // rbtnTest
+            // 
+            this.rbtnTest.AutoSize = true;
+            this.rbtnTest.Location = new System.Drawing.Point(7, 50);
+            this.rbtnTest.Name = "rbtnTest";
+            this.rbtnTest.Size = new System.Drawing.Size(57, 21);
+            this.rbtnTest.TabIndex = 1;
+            this.rbtnTest.TabStop = true;
+            this.rbtnTest.Text = "Test";
+            this.rbtnTest.UseVisualStyleBackColor = true;
+            this.rbtnTest.Click += new System.EventHandler(this.rbtnTest_Click);
+            // 
+            // grpResults
+            // 
+            this.grpResults.Controls.Add(this.lblWpm);
+            this.grpResults.Location = new System.Drawing.Point(599, 130);
+            this.grpResults.Name = "grpResults";
+            this.grpResults.Size = new System.Drawing.Size(144, 77);
+            this.grpResults.TabIndex = 5;
+            this.grpResults.TabStop = false;
+            this.grpResults.Text = "Results";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(645, 259);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(16, 17);
+            this.lblTimer.TabIndex = 6;
+            this.lblTimer.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 356);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.grpResults);
+            this.Controls.Add(this.grpMode);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.txtUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grpMode.ResumeLayout(false);
+            this.grpMode.PerformLayout();
+            this.grpResults.ResumeLayout(false);
+            this.grpResults.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox txtUser;
+        private System.Windows.Forms.RichTextBox txtText;
+        private System.Windows.Forms.Label lblWpm;
+        private System.Windows.Forms.GroupBox grpMode;
+        private System.Windows.Forms.RadioButton rbtnTest;
+        private System.Windows.Forms.RadioButton rbtnPractice;
+        private System.Windows.Forms.GroupBox grpResults;
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
+
---
+++ Typer/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Typer
+{
+    public partial class Form1 : Form
+    {
+        int cur_pos_user = -1, cur_pos_word = -1, word_index;
+        string cur_text = "", old_text = "";
+        string[] word_list;
+        Stopwatch t1 = new Stopwatch();
+
+        int timer_cnt = 60;
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        void timerTick(object sender, EventArgs e)
+        {
+            --timer_cnt;
+            if (timer_cnt == 0)
+            {
+                timer.Stop();
+                if (test_done == false)
+                    lblWpm.Text = "Failed";
+            }
+            lblTimer.Text = timer_cnt.ToString();
+        }
+
+        
+       
+
+        bool test_done;
+
+        string generateText()
+        {
+            t1.Start();
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var textFile = Path.Combine(currentDirectory, "text.txt");
+           // string textFile = @"D:\VS Project\TyperNew\text.txt";
+            List<string> textList = File.ReadAllLines(textFile).ToList();
+            Random rand = new Random();
+            string newText = textList[rand.Next(0, textList.Count)];
+            word_list = newText.Split(' ');
+            word_index = 0;
+
+            cur_pos_user = -1; //Reset posistions
+            cur_pos_word = -1;
+            txtUser.Text = "";
+            cur_text = "";
+            return newText;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Tick += new EventHandler(timerTick);
+            txtText.Text = generateText();
+        }
+
+        void removeText()
+        {
+            if (txtUser.Text == "Type text here...")
+            {
+                txtUser.Text = "";
+                cur_text = "";
+            }
+        }
+
+        private void txtUser_Enter_1(object sender, EventArgs e)
+        {
+            removeText();
+        }
+
+        int calculateTime()
+        {
+            // int words = txtText.Text.Length / 5;
+            //Console.WriteLine(words);
+            return (txtText.Text.Length / 40) * 15;
+        }
+
+        void setTimer()
+        {
+            txtUser.Focus();
+            txtText.Text = generateText();
+
+            test_done = false;
+            timer_cnt = calculateTime();
+            //Console.WriteLine(timer_cnt);
+
+            timer.Interval = 1000; // 1 second
+            timer.Start();
+
+            lblTimer.Text = timer_cnt.ToString();
+        }
+        private void rbtnTest_Click(object sender, EventArgs e)
+        {
+            // cur_pos_user = -1;
+            // cur_pos_word = -1;
+            // word_index = 0;
+
+            setTimer();
+
+            
+        }
+
+        private void rbtnPractice_Click(object sender, EventArgs e)
+        {
+            //cur_pos_user = -1, cur_pos_word = -1, word_index = 0;
+           
+            lblTimer.Text = "0";
+            timer.Stop();
+        }
+
+        void addText()
+        {
+            if (string.IsNullOrWhiteSpace(txtUser.Text))
+            {
+                txtUser.Text = "Type text here...";
+                old_text = "";
+                cur_text = "";
+            }
+
+        }
+        private void txtUser_Leave_1(object sender, EventArgs e)
+        {
+        //    addText();
+        }
+
+        private void txtUser_TextChanged(object sender, EventArgs e)
+        {
+            old_text = cur_text;
+            cur_text = txtUser.Text;
+
+            if (old_text.Length > txtText.Text.Length || cur_text.Length > txtText.Text.Length)
+                return;
+
+            if (cur_text.Length > old_text.Length) //Check if current user-char == text-char
+            {
+                ++cur_pos_user;
+                if (word_list[word_index].Length + 1 < cur_text.Length)
+                    return;
+                ++cur_pos_word;
+                txtText.SelectionStart = cur_pos_word;
+                txtText.SelectionLength = 1;
+
+                if (cur_text[cur_pos_user] == txtText.Text[cur_pos_word]) //True -> Green
+                {
+                    if (txtUser.Text[cur_pos_user] == ' ')
+                    {
+                        if (txtUser.Text == word_list[word_index] + ' ')
+                        {
+                            cur_text = "";
+                            cur_pos_user = -1;
+                            txtUser.Text = "";
+                            ++word_index;
+                        }
+                    }
+                    txtText.SelectionColor = Color.Green;
+                }
+                else //False -> Red
+                {
+                    if (txtText.Text[cur_pos_word] == ' ')
+                        txtText.SelectionBackColor = Color.Red;
+                    else txtText.SelectionColor = Color.Red;
+                }
+            }
+            else //Remove colored-character
+            {
+                if (word_list[word_index].Length >= cur_text.Length)
+                {
+                    txtText.SelectionStart = cur_pos_word;
+                    txtText.SelectionLength = 1;
+                    if (txtText.Text[cur_pos_word] == ' ')
+                        txtText.SelectionBackColor = Color.White;
+                    else txtText.SelectionColor = Color.Black;
+                    --cur_pos_word;
+                }
+                --cur_pos_user;
+            }
+
+            if (cur_pos_word == txtText.Text.Length - 1) //Text finished
+            {
+               
+                t1.Stop(); //Get WPM
+                if (lblWpm.Text != "Failed")
+                {
+                    float words = txtText.Text.Length / 5;
+                    lblWpm.Text = ((int)((words / (float)t1.Elapsed.TotalSeconds) * 60)).ToString() + " WPM";
+                }
+                t1.Reset();
+                test_done = true;
+                if (timer_cnt != 0) test_done = true;
+                timer.Stop();
+
+                //    cur_pos_user = -1; //Generate new text
+                //    cur_pos_word = -1;
+
+                if (rbtnTest.Checked)
+                    setTimer();
+                else txtText.Text = generateText();
+            //    txtUser.Text = "";
+            //    cur_text = "";
+
+            }
+        }
+    }
+}
